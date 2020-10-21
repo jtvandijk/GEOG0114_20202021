479327aabde6f9be1a281d501426d0c2fdb1cc10
JT <jtvandijk@sun.ac.za>
2020-10-21 14:03:28 +0100

    basic toc css

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
+# render bookdown
 set -ev
-
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
---
+++ _push.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# commit message
+commit=$1
+
+# build bookdown
+bash _build.sh
+
+# add files
+git add .
+
+# git commit
+git commit -m "$commit"
+
+# git push
+git push origin master
